da0f5eeb205af69eb069b38d78ed8de0efe4492e
ano <250081373@qq.com>
2017-05-29 21:06:30 +0800

    fix mispell

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -2,14 +2,14 @@
 
 echo "build for newifi mini"
 cp configs/templates/newifimini_mini.config .config
-cp configs/uclibc-mipsel.config configs/boards/NEWIFI-MINI/libc.config
+cp configs/boards/uclibc-mipsel.config configs/boards/NEWIFI-MINI/libc.config
 ./clear_tree
 ./build_firmware
 mv images/NEWIFI-MINI_*.trx /media/Storage/workspace/router/firmware/
 
 echo "build for newifi y1s"
 cp configs/templates/newifiy1s_mini.config .config
-cp configs/uclibc-mipsel.config configs/boards/NEWIFI-Y1S/libc.config
+cp configs/boards/uclibc-mipsel.config configs/boards/NEWIFI-Y1S/libc.config
 echo "build GWAN"
 cp configs/boards/NEWIFI-Y1S/Gboard.h configs/boards/NEWIFI-Y1S/board.h
 cp configs/boards/NEWIFI-Y1S/Gkernel-3.4.x.config configs/boards/NEWIFI-Y1S/kernel-3.4.x.config
@@ -25,14 +25,14 @@ mv images/NEWIFI-Y1S_*.trx /media/Storage/workspace/router/firmware/MNEWIFI-Y1S_
 
 echo "build for newifi d1"
 cp configs/templates/newifid1_mini.config .config
-cp configs/uclibc-mipsel.config configs/boards/NEWIFI-D1/libc.config
+cp configs/boards/uclibc-mipsel.config configs/boards/NEWIFI-D1/libc.config
 ./clear_tree
 ./build_firmware
 mv images/NEWIFI-D1_*.trx /media/Storage/workspace/router/firmware/
 
 echo "build for psg1218"
 cp configs/templates/psg1218_mini.config .config
-cp configs/uclibc-mipsel.config configs/boards/PSG1218/libc.config
+cp configs/boards/uclibc-mipsel.config configs/boards/PSG1218/libc.config
 ./clear_tree
 ./build_firmware
 mv images/PSG1218_*.trx /media/Storage/workspace/router/firmware/
