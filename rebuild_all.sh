e34ab3ded48c0d43845a8ebc1d9fe30d6c7031a2
ano <250081373@qq.com>
2017-05-29 19:36:35 +0800

    fix

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -13,13 +13,13 @@ cp configs/boards/NEWIFI-Y1S/Gboard.h configs/boards/NEWIFI-Y1S/board.h
 cp configs/boards/NEWIFI-Y1S/Gkernel-3.4.x.config configs/boards/NEWIFI-Y1S/kernel-3.4.x.config
 ./clear_tree
 ./build_firmware
-mv images/*.trx /media/Storage/workspace/router/firmware/GNEWIFI-Y1S_3.4.3.9-099.trx
+mv images/NEWIFI-Y1S_*.trx /media/Storage/workspace/router/firmware/GNEWIFI-Y1S_3.4.3.9-099.trx
 echo "build MWAN"
 cp configs/boards/NEWIFI-Y1S/Mboard.h configs/boards/NEWIFI-Y1S/board.h
 cp configs/boards/NEWIFI-Y1S/Mkernel-3.4.x.config configs/boards/NEWIFI-Y1S/kernel-3.4.x.config
 ./clear_tree
 ./build_firmware
-mv images/*.trx /media/Storage/workspace/router/firmware/MNEWIFI-Y1S_3.4.3.9-099.trx
+mv images/NEWIFI-Y1S_*.trx /media/Storage/workspace/router/firmware/MNEWIFI-Y1S_3.4.3.9-099.trx
 
 echo "build for newifi d1"
 cp configs/templates/newifid1_mini.config .config
