13d5ee993d4cdfedd401fd84c7bfe4d5bf904125
ano <anoidgit@users.noreply.github.com>
2017-05-29 15:26:30 +0800

    Update and rename useO3.py to redirectOptimize.py

--- tools/redirectOptimize.py
+++ tools/redirectOptimize.py
@@ -3,17 +3,23 @@
 import os
 import sys
 
-def useO3(fname):
+def rfile(fname, old, new):
 	cache = []
 	rwt = False
+	ostr="-"+old
+	nstr="-"+new
+	fstr1=ostr+" "
+	fstr2=ostr+"\""
+	fstr3="="+ostr
+	fstr4="\""+ostr
 	try:
 		with open(fname) as f:
 			for line in f:
 				tmp = line.strip("\r\n")
 				tmp = tmp.decode("utf-8")
 				tt = tmp.strip()
-				if tmp.find(" -O2 ")!=-1 or tt.endswith("-O2") or tt.endswith("-O2\"") or tt.startswith("-O2") or tmp.find("=-O2")!=-1 or tmp.find("\"-O2")!=-1:
-					tmp = tmp.replace("-O2","-O3")
+				if tmp.find(fstr1)!=-1 or tt.endswith(ostr) or tt.endswith(fstr2) or tt.startswith(ostr) or tmp.find(fstr3)!=-1 or tmp.find(fstr4)!=-1:
+					tmp = tmp.replace(ostr,nstr)
 					rwt = True
 				cache.append(tmp)
 	except:
@@ -28,16 +34,16 @@ def useO3(fname):
 			f.write(cache)
 	return rwt
 
-def walkdir(dname):
+def walkdir(dname, old, new):
 	count = 0
 	for root, dirs, files in os.walk(dname):
 		for file in files:
 			if file=="Makefile":
-				rp = useO3(os.path.join(root,file))
+				rp = rfile(os.path.join(root,file), old, new)
 				if rp:
 					count += 1
 	if count > 0:
 		print(str(count)+" files edited")
 
 if __name__ == "__main__":
-	walkdir(sys.argv[1].decode("utf-8"))
+	walkdir(sys.argv[1].decode("utf-8"), sys.argv[2].decode("utf-8"), sys.argv[3].decode("utf-8"))
