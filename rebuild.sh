8138204492f317792473367bb4ae8672dabb463e
ano <anoidgit@users.noreply.github.com>
2017-06-25 08:51:00 +0800

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -5,8 +5,8 @@ cd ..
 pypy redirectOptimize.py rt-n56u O2 O3
 cd padavan
 git pull
-chmod a+x install.sh
-bash install.sh
+chmod a+x install_O3.sh
+bash install_O3.sh
 
 cd ../rt-n56u/trunk
 ./rebuild_all.sh
